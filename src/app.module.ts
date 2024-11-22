ca880aacd87549a6ece8446867b57dae31be4e43
Alessandro Bocci <alessandro.bocci@frescofrigo.it>
2024-11-22 22:36:14 +0100

    feat(config): add TypeORM and ConfigModule setup to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getTypeOrmConfig } from './database/database.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: getTypeOrmConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
