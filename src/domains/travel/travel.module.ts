3cb2fd7a53ffedb9a25712c8cd224e47fa3814ad
Alessandro Bocci <alessandro.bocci@frescofrigo.it>
2024-11-25 10:48:21 +0100

    feat(travel): add validator to module

--- src/domains/travel/travel.module.ts
+++ src/domains/travel/travel.module.ts
@@ -4,9 +4,10 @@ import { TravelService } from './travel.service';
 import { TravelResolver } from './travel.resolver';
 import { Travel } from './entities/travel.entity';
 import { Mood } from './entities/mood.entity';
+import { TravelValidator } from './travel.validator';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Travel, Mood])],
-  providers: [TravelService, TravelResolver],
+  providers: [TravelService, TravelResolver, TravelValidator],
 })
 export class TravelModule {}
