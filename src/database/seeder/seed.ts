70b32a51301d7a36cb8830f1278885bd214fa15a
Alessandro Bocci <alessandro.bocci@frescofrigo.it>
2024-11-26 09:14:48 +0100

    feat(seed): add user data to seed

--- src/database/seeder/seed.ts
+++ src/database/seeder/seed.ts
@@ -1,10 +1,13 @@
 import { DataSource } from 'typeorm';
+
 import { Mood } from '../../domains/travel/entities/mood.entity';
 import { Travel } from '../../domains/travel/entities/travel.entity';
+import { User } from '../../domains/user/user.entity';
 import { MoodType } from '../../domains/travel/enums/mood-type.enum';
 
 import { createDataSource } from './datasource';
 import * as travelData from './seed-data/travels.json';
+import * as userData from './seed-data/users.json';
 
 const seedTravelData = async (dataSource: DataSource) => {
   const travelRepository = dataSource.getRepository(Travel);
@@ -26,12 +29,24 @@ const seedTravelData = async (dataSource: DataSource) => {
   }
 };
 
+const seedUserData = async (dataSource: DataSource) => {
+  const userRepository = dataSource.getRepository(User);
+
+  for (const user of userData) {
+    console.log(`Seeding user: ${user.id}`);
+    const newTravel = userRepository.create(user);
+
+    await userRepository.save(newTravel);
+  }
+};
+
 (async () => {
   const dataSource = createDataSource();
   await dataSource.initialize();
 
   try {
     await seedTravelData(dataSource);
+    await seedUserData(dataSource);
     console.log('Seed completed with success!');
     await dataSource.destroy();
   } catch (error) {
