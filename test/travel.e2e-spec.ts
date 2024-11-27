f3a5c8d48ad74489f6ac770f0d4b2edc4dde627a
Alessandro Bocci <alessandro.bocci@frescofrigo.it>
2024-11-27 22:54:45 +0100

    fix(travel): e2e test

--- test/travel.e2e-spec.ts
+++ test/travel.e2e-spec.ts
@@ -1,41 +1,15 @@
 import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppModule } from '../src/app.module';
-import { Travel } from '../src/domains/travel/entities/travel.entity';
 import { ConfigService } from '@nestjs/config';
 import { getTypeOrmConfig } from '../src/config/database.config';
-import { MoodType } from '../src/domains/travel/entities/mood.entity';
+import { runSeed, truncateSeed } from './utils';
 
 describe('TravelResolver (e2e)', () => {
   let app: INestApplication;
-  let travelRepository: Repository<Travel>;
-
-  let travel: Travel;
-
-  const travelTestData = {
-    id: '1a2b34cd-5ef6-7a89-01b2-34c5d6e7f8a9',
-    slug: 'a-test-travel',
-    name: 'A Test Travel',
-    description:
-      'A test travel description here to explain better a travel experience',
-    startingDate: new Date(),
-    endingDate: new Date(),
-    price: 119900,
-    availableSeats: 10,
-    totalNumberOfSeats: 20,
-    moods: [
-      {
-        id: 1,
-        travel: null,
-        mood: MoodType.CULTURE,
-        score: 50,
-      },
-    ],
-  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -51,9 +25,8 @@ describe('TravelResolver (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
 
-    travelRepository = moduleFixture.get(getRepositoryToken(Travel));
-
-    travel = await travelRepository.save(travelTestData);
+    truncateSeed();
+    runSeed();
   });
 
   afterAll(async () => {
@@ -62,12 +35,13 @@ describe('TravelResolver (e2e)', () => {
 
   describe('Queries', () => {
     it('should fetch travel by ID', async () => {
+      const travelId = 'd408be33-aa6a-4c73-a2c8-58a70ab2ba4d';
       const response = await request(app.getHttpServer())
         .post('/graphql')
         .send({
           query: `
             query {
-              getTravelById(id: "${travel.id}") {
+              getTravelById(id: "${travelId}") {
                 id
                 name
                 availableSeats
@@ -77,11 +51,10 @@ describe('TravelResolver (e2e)', () => {
         })
         .expect(200);
 
-      expect(response.body.data.getTravelById.id).toEqual(travel.id);
-      expect(response.body.data.getTravelById.name).toEqual(travel.name);
-      expect(response.body.data.getTravelById.availableSeats).toEqual(
-        travel.availableSeats,
-      );
+      const { id, name, availableSeats } = response.body.data.getTravelById;
+      expect(id).toEqual('d408be33-aa6a-4c73-a2c8-58a70ab2ba4d');
+      expect(name).toEqual('Jordan 360°');
+      expect(availableSeats).toEqual(10);
     });
 
     it('should return an error for a non-existent travel ID', async () => {
@@ -102,12 +75,13 @@ describe('TravelResolver (e2e)', () => {
     });
 
     it('should fetch travel by slug', async () => {
+      const travelSlug = 'jordan-360';
       const response = await request(app.getHttpServer())
         .post('/graphql')
         .send({
           query: `
             query {
-              getTravelBySlug(slug: "${travel.slug}") {
+              getTravelBySlug(slug: "${travelSlug}") {
                 id
                 name
               }
@@ -116,8 +90,9 @@ describe('TravelResolver (e2e)', () => {
         })
         .expect(200);
 
-      expect(response.body.data.getTravelBySlug.id).toBe(travelTestData.id);
-      expect(response.body.data.getTravelBySlug.name).toBe(travelTestData.name);
+      const { id, name } = response.body.data.getTravelBySlug;
+      expect(id).toBe('d408be33-aa6a-4c73-a2c8-58a70ab2ba4d');
+      expect(name).toBe('Jordan 360°');
     });
 
     it('should return an error for a non-existent slug', async () => {
@@ -158,66 +133,13 @@ describe('TravelResolver (e2e)', () => {
         })
         .expect(200);
 
-      expect(response.body.data.getAllTravels.data.length).toBeGreaterThan(0);
-      expect(response.body.data.getAllTravels.total).toBe(24);
-      expect(response.body.data.getAllTravels.page).toBe(1);
-    });
-  });
-
-  describe('Mutations', () => {
-    it('should increase available seats', async () => {
-      const response = await request(app.getHttpServer())
-        .post('/graphql')
-        .send({
-          query: `
-            mutation {
-              increaseAvailableSeats(id: "${travel.id}", seats: 5) {
-                id
-                availableSeats
-              }
-            }
-          `,
-        })
-        .expect(200);
-
-      expect(response.body.data.increaseAvailableSeats.availableSeats).toBe(15);
-    });
-
-    it('should decrease available seats', async () => {
-      const response = await request(app.getHttpServer())
-        .post('/graphql')
-        .send({
-          query: `
-            mutation {
-              decreaseAvailableSeats(id: "${travel.id}", seats: 5) {
-                id
-                availableSeats
-              }
-            }
-          `,
-        })
-        .expect(200);
-
-      expect(response.body.data.decreaseAvailableSeats.availableSeats).toBe(10);
-    });
-
-    it('should return an error when decreasing available seats below 0', async () => {
-      const response = await request(app.getHttpServer())
-        .post('/graphql')
-        .send({
-          query: `
-            mutation {
-              decreaseAvailableSeats(id: "${travel.id}", seats: 20) {
-                id
-              }
-            }
-          `,
-        })
-        .expect(200);
-
-      expect(response.body.errors[0].message).toBe(
-        'Not enough available seats',
-      );
+      const { data, total, page, pageSize, totalPages } =
+        response.body.data.getAllTravels;
+      expect(data.length).toBeGreaterThan(0);
+      expect(total).toBe(23);
+      expect(page).toBe(1);
+      expect(pageSize).toBe(10);
+      expect(totalPages).toBe(3);
     });
   });
 });
